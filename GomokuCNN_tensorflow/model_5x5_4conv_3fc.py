f0d02ad907ee6c3017ea86a2eda8488120f625f5
ManjunGim <uh1607@naver.com>
2017-08-05 08:44:11 +0000

    Amending

--- GomokuCNN_tensorflow/model_5x5_4conv_3fc.py
+++ GomokuCNN_tensorflow/model_5x5_4conv_3fc.py
@@ -101,10 +101,10 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-#    saver.restore(sess, os.path.join(cur_dir, 'tmp_no_pooling/6_28000_model.ckpt'))
-#    print("Model restored.")
-    for epoch in range(1, 30):
-        if epoch == 31:
+    saver.restore(sess, os.path.join(cur_dir, 'tmp_4conv3fc_no_pooling/3_28000_model.ckpt'))
+    print("Model restored.")
+    for epoch in range(4, 31):
+        if epoch == 32:
             for i in range(8000, 28000):
                 batch_start = i * 50
                 if i % 100 == 0:
@@ -120,18 +120,19 @@ with tf.Session() as sess:
             x_train, y_train = zip(*train)
             for i in range(28000):
                 batch_start = i * 50
-                if i % 100 == 0:
+                if (i + 1) % 14000 == 0:
                     train_accuracy = accuracy.eval(feed_dict={x: x_train[batch_start:batch_start + 50], y_answer: y_train[batch_start:batch_start + 50], keep_prob: 1.0})
                     loss = cross_entropy.eval(feed_dict={x: x_train[batch_start:batch_start + 50], y_answer: y_train[batch_start:batch_start + 50], keep_prob: 1.0})
-                    print('step %d, training accuracy %g, loss %g' % (i, train_accuracy, loss))
+                    print('step %d, training accuracy %g, loss %g' % (i + 1, train_accuracy, loss))
                 sess.run(train_step, feed_dict={x: x_train[batch_start:batch_start + 50], y_answer: y_train[batch_start:batch_start + 50], keep_prob: 0.5})
                 if (i + 1) % 28000 == 0:
-                   save_path = saver.save(sess, cur_dir + '/tmp_4conv3fc__no_pooling/' + str(epoch) + '_' + str(i + 1) + '_model.ckpt')
+                   save_path = saver.save(sess, cur_dir + '/tmp_4conv3fc_no_pooling/' + str(epoch) + '_' + str(i + 1) + '_model.ckpt')
                    print("Model saved in file: %s" % save_path)
+
         list_test_accuracy = np.zeros(0, dtype=np.float)
         for i in range(5):
-            test_index = i * 1000
-            test_accuracy = accuracy.eval(feed_dict={x: x_test[test_index:test_index + 1000], y_answer: y_test[test_index:test_index + 1000], keep_prob: 1.0})
+            test_index = i * 500
+            test_accuracy = accuracy.eval(feed_dict={x: x_test[test_index:test_index + 500], y_answer: y_test[test_index:test_index + 500], keep_prob: 1.0})
             list_test_accuracy = np.append(list_test_accuracy, test_accuracy)
             print('epoch: %d, test accuracy: %g' % (epoch, test_accuracy))
         print("average test accuracy: %g" % list_test_accuracy.mean())
